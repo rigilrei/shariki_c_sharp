a500ebb29e4254c188ada912250e619d9d3c7652
rigilrei <138469843+rigilrei@users.noreply.github.com>
2025-06-19 22:39:01 +0300

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Shariki2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        static App()
+        {
+            // Инициализация SQLite
+            SQLitePCL.Batteries_V2.Init();
+        }
+    }
+}
